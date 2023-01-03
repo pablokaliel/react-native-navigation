5405678a826ff5c92d473abad0a403238f02037c
Pablo <pablo.kalyell.441@gmail.com>
2023-01-03 10:12:11 -0200

    add styles in other past

--- src/screens/screenB/index.tsx
+++ src/screens/screenB/index.tsx
@@ -8,7 +8,7 @@ import {
   StyleSheet,
   SafeAreaView,
 } from "react-native";
-
+import {styles} from './styles'
 
 type ParamsProps = {
   name: string;
@@ -44,28 +44,3 @@ export default function ScreenB() {
   );
 }
 
-const styles = StyleSheet.create({
-  Container: {
-    flex: 1,
-    backgroundColor: "#082b44",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  ViewInfo: {
-    alignItems: "center",
-    width: "100%",
-    justifyContent: "center",
-    flexDirection: "row",
-  },
-  Info: { color: "#fff", fontSize: 14 },
-  InfoText: { marginLeft: 10, fontSize: 16, color: "#00eeff" },
-  Button: {
-    alignItems: "center",
-    backgroundColor: "#0a95d6ca",
-    padding: 10,
-    width: 300,
-    marginTop: 16,
-    borderRadius: 16,
-  },
-  TextButton: { color: "#fff" },
-});
